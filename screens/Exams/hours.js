e15cba888f32218f0b8ddaf90073974aaa918ada
jenish <jenish@closedigit.com>
2023-08-12 19:32:35 +0530

    added wishlists week slots

--- screens/Exams/hours.js
+++ screens/Exams/hours.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -18,52 +18,49 @@ import { useNavigation } from "@react-navigation/native";
 import { connect, useSelector } from "react-redux";
 import { createExamWishlist } from "../../services/ExamWishlist/actions";
 import { Toast } from "react-native-toast-message/lib/src/Toast";
+import MonthPicker from "react-native-month-year-picker";
 
 const ExamHours = (props) => {
-    const navigation = useNavigation()
-    const userData = useSelector((state) => state?.authReducer?.data?.data);
+  const navigation = useNavigation()
+  const userData = useSelector((state) => state?.authReducer?.data?.data);
 
   const [date, setDate] = useState(moment().toDate());
   const [dateOpen, setDateOpen] = useState(false);
   const [loading, setLoading] = useState(false);
   const [hours, setHours] = useState(0)
   const [studentsData, setStudentsData] = useState({})
+  const [weeksData, setWeeksData] = useState([])
+  const [selectedWeek, setSelectedWeek] = useState()
+  const [month, setMonth] = useState(new Date())
+  const [showPicker, setShowPicker] = useState(false)
 
   const handleUpdate = async () => {
-    try{
-        setLoading(true)
+    try {
+      setLoading(true)
 
-        if(Number.isNaN(parseInt(hours)) || hours <= 0){
-           return Toast.show({
-            type: "error",
-            text1: "Veuillez entrer des heures valides!"
-          }) 
-          //  return ToastAndroid.show("Veuillez entrer des heures valides!", ToastAndroid.SHORT) 
-        }
-
-        const payload = {
-            instructor_id: userData?.id,
-            student_id: studentsData?.id,
-            date_start: date,
-            date_end: moment(date).add(hours, 'hours').toDate(),
-            status: 'pending'
-        }
-        await props?.createExamWishlist(payload)
-        Toast.show({
-          type: "success",
-          text1: "Liste de souhaits d'examen ajoutée"
-        })
-        // ToastAndroid.show("Liste de souhaits d'examen ajoutée", ToastAndroid.SHORT)
-        navigation.navigate("studentsList")
-    }catch(error){
+      const payload = {
+        instructor_id: userData?.id,
+        student_id: studentsData?.id,
+        date_start: moment(weeksData[selectedWeek]?.startDate, "YYYY-MM-DD"),
+        date_end: moment(weeksData[selectedWeek]?.endDate, "YYYY-MM-DD"),
+        status: 'pending'
+      }
+      await props?.createExamWishlist(payload)
+      Toast.show({
+        type: "success",
+        text1: "Liste de souhaits d'examen ajoutée"
+      })
+      // ToastAndroid.show("Liste de souhaits d'examen ajoutée", ToastAndroid.SHORT)
+      navigation.navigate("studentsList")
+    } catch (error) {
       Toast.show({
         type: "error",
         text1: "Erreur lors de l'ajout de la liste de souhaits d'examen"
       })
-        // ToastAndroid.show("Erreur lors de l'ajout de la liste de souhaits d'examen", ToastAndroid.SHORT)
-        console.log(error)
-    }finally{
-        setLoading(false)
+      // ToastAndroid.show("Erreur lors de l'ajout de la liste de souhaits d'examen", ToastAndroid.SHORT)
+      console.log(error)
+    } finally {
+      setLoading(false)
     }
 
   }
@@ -73,12 +70,52 @@ const ExamHours = (props) => {
   }
 
   useEffect(() => {
-    if(props?.route?.params?.data?.id){
-        setStudentsData(props?.route?.params?.data)
-    }else{
-        setStudentsData({})
+    if (props?.route?.params?.data?.id) {
+      setStudentsData(props?.route?.params?.data)
+    } else {
+      setStudentsData({})
     }
   }, [props?.route?.params?.data])
+
+  useEffect(() => {
+    // Get the current date
+    const currentDate = moment(month);
+
+    // Get the start of the current month
+    const startOfMonth = currentDate.clone().startOf('month');
+
+    // Get the end of the current month
+    const endOfMonth = currentDate.clone().endOf('month');
+
+    // Initialize an array to store the weeks
+    const weeks = [];
+
+    // Start from the beginning of the month and add a week at a time until we reach the end of the month
+    let currentWeekStart = startOfMonth.clone();
+    while (currentWeekStart.isBefore(endOfMonth)) {
+      const currentWeekEnd = currentWeekStart.clone().endOf('week');
+      // Make sure the last day of the last week is within the current month
+      const lastWeekEnd = endOfMonth.isBefore(currentWeekEnd) ? endOfMonth : currentWeekEnd;
+      weeks.push({
+        startDate: currentWeekStart.format('YYYY-MM-DD'),
+        endDate: lastWeekEnd.format('YYYY-MM-DD'),
+      });
+      currentWeekStart = currentWeekEnd.add(1, 'day'); // Move to the next week
+    }
+
+    setWeeksData(weeks)
+
+  }, [month])
+
+  const onValueChange = useCallback(
+    (event, newDate) => {
+      const selectedDate = newDate || date;
+
+      setShowPicker(false);
+      setMonth(selectedDate);
+    },
+    [date, showPicker],
+  );
 
   return (
     <SafeAreaView>
@@ -96,45 +133,54 @@ const ExamHours = (props) => {
               </Text>
             </View>
             <View style={{ paddingHorizontal: 10 }} >
-            <View style={{ marginTop: 50 }} >
-              <Text style={styles.labelText}>Exam start time</Text>
-              <TouchableOpacity
-                onPress={() => {
-                  if (!loading) {
-                    setDateOpen(true);
-                  }
-                }}
-              >
-                <Text style={{ ...styles.textWithBox, marginBottom: 0 }}>
-                  {moment(date).format("DD/MM/YYYY hh:mm A")}
-                </Text>
-              </TouchableOpacity>
-              <DatePicker
-                modal
-                mode="datetime"
-                open={dateOpen}
-                date={date}
-                onConfirm={(date) => {
-                  setDateOpen(false);
-                  setDate(date);
-                }}
-                onCancel={() => {
-                  setDateOpen(false);
-                }}
-              />
-            </View>
-            <View style={{ marginTop: 20 }} >
-              <Text style={styles.labelText}>Hours</Text>
-              <TextInput
-                editable={!loading}
-                  value={`${hours}`}
-                  onChangeText={setHours}
-                  style={styles.textInput}
-                  keyboardType="numeric"
-                  maxLength={4}
-              />
-            </View>
-            <View
+              {
+                showPicker &&
+                <View>
+                  <MonthPicker
+                    value={month}
+                    onChange={onValueChange}
+                    mode='full'
+                    locale="en"
+                    okButton="Confirm"
+                    cancelButton="Abort"
+                  />
+                </View>
+              }
+              <View style={{ marginTop: 20 }} >
+                <Text style={styles.labelText}>Select month</Text>
+                <TouchableOpacity
+                        onPress={() => {
+                          if (!loading) {
+                            setShowPicker(true);
+                          }
+                        }}
+                      >
+                        <Text style={{ ...styles.textWithBox, marginBottom: 0}}>
+                          {moment(month).format("MM-YYYY")}
+                        </Text>
+                      </TouchableOpacity>
+              </View>
+              <View style={{ marginTop: 20 }} >
+                <Text style={styles.labelText}>Exam start time</Text>
+                <View style={{ gap: 20 }} >
+                  {weeksData?.map((item, index) => {
+                    return (
+                      <TouchableOpacity
+                        onPress={() => {
+                          if (!loading) {
+                            setSelectedWeek(index);
+                          }
+                        }}
+                      >
+                        <Text style={{ ...styles.textWithBox, marginBottom: 0, backgroundColor: selectedWeek === index ? colors.cyan : colors.gray }}>
+                          {item?.startDate} - {item?.endDate}
+                        </Text>
+                      </TouchableOpacity>
+                    )
+                  })}
+                </View>
+              </View>
+              <View
                 style={{
                   flexDirection: "row",
                   justifyContent: "space-evenly",
@@ -173,7 +219,7 @@ const ExamHours = (props) => {
 
 const styles = StyleSheet.create({
   examSteps: {
-    marginTop: 50,
+    marginTop: 20,
     padding: 16,
     alignItems: "center",
     justifyContent: "flex-start",
@@ -229,13 +275,13 @@ const styles = StyleSheet.create({
 });
 
 const mapStateProps = (state) => {
-    return {
+  return {
 
-    }
+  }
 }
 
 const mapDispatchToProps = {
-    createExamWishlist
+  createExamWishlist
 }
 
 export default connect(mapStateProps, mapDispatchToProps)(ExamHours);
